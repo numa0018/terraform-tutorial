526a3b525176d2c617ca14f2ae7af66ffe3d0b01
Mitsuhito Numa <tnm14893@yahoo.co.jp>
2022-04-07 20:27:57 +0900

    fix(tf): Fix a syntax error.

--- practice/chapter2/main.tf
+++ practice/chapter2/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "test_instance" {
   ami = "ami-0c3fd0f5d3313134a76"
   instance_type = "t3.micro"
-}
-tags = {
-  Name = "test_instance"
+  tags = {
+    Name = "test_instance"
+  }
 }
