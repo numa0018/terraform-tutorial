58fe6ec258279a4858d2f9e895e1c49a5e09a2f1
Mitsuhito Numa <tnm14893@yahoo.co.jp>
2022-04-09 19:39:58 +0900

    feat(tf): Add tf-file for EC2 and SecurityGroup.

---
+++ practice/chapter3/main.tf
@@ -0,0 +1,28 @@
+resource "aws_security_group" "security_group_for_ec2" {
+  name = "security_group_for_ec2"
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress = [ {
+    from_port = 0 
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  } ]
+}
+
+resource "aws_instance" "test_instance" {
+  ami = "ami-0521a4a0a1329ff86"
+  instance_type = "t3.micro"
+  tags = {
+    Name = "hoge_instance"
+  }
+  user_data = <<EOF
+    #!/bin/bash
+    yum install -y httpd
+    systemctl start httpd.service
+EOF
+}
